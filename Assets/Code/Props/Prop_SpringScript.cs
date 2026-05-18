d4c3f41a52a7d3073a0efeaff9a240b10ecf678e
Fernando Martinez Lineros <fermartilin@gmail.com>
2026-05-18 17:58:41 +0200

    Gold Version

--- Assets/Code/Props/Prop_SpringScript.cs
+++ Assets/Code/Props/Prop_SpringScript.cs
@@ -4,6 +4,11 @@ public class Prop_SpringScript : MonoBehaviour
 {
     [SerializeField] private Animator propAnimator;
     [SerializeField] private float pushForce = 35f;
+    [Header("Sound")]
+    [SerializeField] private AudioClip touchSound;
+    [SerializeField] private float minPitch = 0.9f;
+    [SerializeField] private float maxPitch = 1.2f;
+    [SerializeField] private float volume = 1f;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +19,22 @@ public class Prop_SpringScript : MonoBehaviour
             Rigidbody playerRb = other.GetComponent<Rigidbody>();
 
             MovementBooster.BounceUp(playerRb, pushForce);
+            PlayCollectSound();
         }
     }
+
+    private void PlayCollectSound()
+        {
+            if (touchSound == null) return;
+
+            GameObject soundObject = new GameObject("Spring Sound");
+            AudioSource audioSource = soundObject.AddComponent<AudioSource>();
+
+            audioSource.clip = touchSound;
+            audioSource.volume = volume;
+            audioSource.pitch = Random.Range(minPitch, maxPitch);
+            audioSource.Play();
+
+            Destroy(soundObject, touchSound.length / audioSource.pitch);
+        }
 }
